1e2f4350fa12b2012b1684c3eae8c07b0a3ef6ec
Jegan2005-jp <jeganrajp2005@gmail.com>
2026-03-24 01:31:48 +0530

    second upload

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 from werkzeug.utils import secure_filename
 import os
 import io
